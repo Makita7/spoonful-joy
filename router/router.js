be5383c1a08b22c92247a8a7dca02afa39f96d43
sofia-makita <smakita@iadb.org>
2024-04-16 08:58:25 -0300

    first commit2

---
+++ router/router.js
@@ -0,0 +1,17 @@
+
+import { createRouter, createWebHistory } from "vue-router";
+
+
+const routes = [
+    { path: '/', component: },
+]
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+    scrollBehavior(){
+        return { top: 0 }
+    }
+})
+
+export default router;
