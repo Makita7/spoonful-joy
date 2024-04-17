a0b187382e01fb81737b34fd9a6766b449c08d7b
sofia-makita <smakita@iadb.org>
2024-04-17 14:46:40 -0300

    deleting vite content

---
+++ src/components/recipeCard.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <div>card</div>
+</template>
+
+<style scoped lang="scss"></style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,14 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainPage from '@/pages/MainPage.vue'
 
-const routes = [{ path: '/', component: MainPage }]
+const routes = [
+    { path: '/', component: MainPage },
+    { path: '/recipe', component: () => import('/src/pages/RecipePage.vue') },
+    { path: '/edit', component: () => import('/src/pages/EditAddPage.vue') }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-  scrollBehavior() {
-    return { top: 0 }
-  }
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+    scrollBehavior() {
+        return { top: 0 }
+    }
 })
 
 export default router
