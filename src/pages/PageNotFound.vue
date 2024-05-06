4fde2e92652d15cb5925a9f3f93419815ff4125c
sofia-makita <smakita@iadb.org>
2024-05-06 15:50:22 -0300

    404 content page

--- src/pages/PageNotFound.vue
+++ src/pages/PageNotFound.vue
@@ -1,8 +1,28 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-    <div>404</div>
+    <div class="text-center">
+        <h2>404</h2>
+        <p>
+            Page not found,
+            <br />
+            click to go
+            <RouterLink to="/">Home</RouterLink>
+        </p>
+    </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+a {
+    color: var(--black);
+    font-weight: bolder;
+}
+
+h2 {
+    font-weight: bolder;
+    font-size: 5rem;
+    color: var(--coral);
+}
+</style>
