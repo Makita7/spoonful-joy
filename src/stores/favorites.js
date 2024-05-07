b74efa8f305adc4759720f8a0902be3e4c3f31b5
sofia-makita <smakita@iadb.org>
2024-05-07 15:09:08 -0300

    fixing like button

--- src/stores/favorites.js
+++ src/stores/favorites.js
@@ -5,22 +5,35 @@ export const useFavoritesStore = defineStore('favorites', () => {
     let favs = ref([]);
 
     const toggleFavs = (idDish, title) => {
-        if(favs.length > 0){
-            if(favs.map(i => i.id == idDish)){
-                favs.push({
+        if(!undefined && favs.value.length >= 1){
+            if(!favs.value.find(i => i.id == idDish)){
+                favs.value.push({
                     id: idDish,
                     dishName: title,
                     like: true
                 })
             }else{
-                const index = favs.indexOf(i => i.id == idDish);
-                favs = favs.splice(index, 1);
+                const index = favs.value.findIndex(i => i.id === idDish);
+                favs = favs.value.splice(index, 1);
             }
+            console.log('if', favs.value)
+        }else{
+            favs.value.push({
+                id: idDish,
+                dishName: title,
+                like: true
+            })
+            console.log('else push', favs.value)
         }
+        if(favs.value === undefined){
+            favs.value = []
+        }
+        console.log(favs.value)
     }
 
     function checkFavs(idDish){
-        if(favs.length > 0){
+        if(favs.value.length > 1){
+            console.log(`checked ${favs.map(i => i.id == idDish)[0]}`)
             return favs.map(i => i.id == idDish)[0];
         }
     }
